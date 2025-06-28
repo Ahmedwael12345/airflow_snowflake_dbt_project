78039e1b1b41fc366b2e8420223d374598b2b374
Ahmed Wael <ahmedwael@192.168.1.21>
2025-06-28 11:25:20 +0300

    Add Data Files folder

--- dbt/snowflake_data_project/models/staging/stg_customer.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT 
-id AS customer_id, 
-name AS customer_name ,
-email, 
-country
-FROM {{ source('raw_data', 'customers') }}
--- dbt/snowflake_data_project/models/staging/stg_orders.sql
+++
@@ -1,2 +0,0 @@
-select id as order_id , customer_id,order_date,status as order_status
- from {{source('raw_data','orders')}}
